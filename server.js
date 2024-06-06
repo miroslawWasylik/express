980d962e19bbcbfffc2e9ea634b58681ce2a2abc
ladenisko <ladenmail@gmail.com>
2024-06-06 18:08:20 +0200

    add some changes

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+	res.send({
+		id: 1,
+		name: 'Sir Isaac Newton'
+	});
+});
+
+app.get('/messages', (req, res) => {
+	res.send('<ul><li>Helloo Albert!</ul></li>');
+});
+
+app.post('/messages', (req, res) => {
+	console.log('Updating messages...');
+});
+
 app.listen(PORT, () => {
 	console.log(`listening on port ${PORT}...`);
 });
