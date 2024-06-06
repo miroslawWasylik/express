570eaef8599b1dfc87be715309bb292f7a60d640
ladenisko <ladenmail@gmail.com>
2024-06-06 21:17:55 +0200

    add some html, css, and jpg files

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 function getMessages(req, res) {	
-	res.sendFile(path.join(__dirname, '..', 'public', '3551739.jpg'));
+	res.sendFile(path.join(__dirname, '..', 'public', 'images', '3551739.jpg'));
 	// res.send('<ul><li>Helloo Albert!</ul></li>');
 }
 
